ebad1978850a574fc3d98dbb45c61f1be6aa1a18
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-01 08:36:03 +0300

    method refactor

--- src/main/java/pages/ParseMethods.java
+++ src/main/java/pages/ParseMethods.java
@@ -6,35 +6,40 @@ import io.restassured.response.Response;
 
 public class ParseMethods {
 
-    public static JsonObject getJsonFromResponse (Response response) {
+    public static JsonObject getJsonFromResponse(Response response) {
         return new JsonParser().parse(response.getBody().asString()).getAsJsonObject();
     }
 
-    public static String getFirstSourceInfo (Response response) {
-        return getJsonFromResponse(response)
-                .get("sources").getAsJsonArray()
-                .get(0).getAsJsonObject()
+    public static String getFirstSourceInfo(Response response) {
+        return getFirstSource(response)
                 .get("name").getAsString();
     }
 
-    public static String getFirstSourceDescription (Response response) {
-        return getJsonFromResponse(response)
-                .get("sources").getAsJsonArray()
-                .get(0).getAsJsonObject()
+    public static String getFirstSourceDescription(Response response) {
+        return getFirstSource(response)
                 .get("description").getAsString();
     }
 
-    public static String getFirstBelarusNewsTitle (Response response) {
-        return getJsonFromResponse(response)
-                .get("articles").getAsJsonArray()
-                .get(0).getAsJsonObject()
+    public static String getFirstBelarusNewsTitle(Response response) {
+        return getFirstNews(response)
                 .get("title").getAsString();
     }
-    public static String getFirstRUNewsSourceName (Response response) {
-        return getJsonFromResponse(response)
-                .get("articles").getAsJsonArray()
-                .get(0).getAsJsonObject()
+
+    public static String getFirstRUNewsSourceName(Response response) {
+        return getFirstNews(response)
                 .get("source").getAsJsonObject()
                 .get("name").getAsString();
+    }
+
+    private static JsonObject getFirstSource(Response response) {
+        return getJsonFromResponse(response)
+                .get("sources").getAsJsonArray()
+                .get(0).getAsJsonObject();
+    }
+
+    private static JsonObject getFirstNews(Response response) {
+        return getJsonFromResponse(response)
+                .get("articles").getAsJsonArray()
+                .get(0).getAsJsonObject();
     }
 }
